c26fb3066d5ccbaf62cdfcadb018ed88392f1ad7
LostKid123 <52243508+DiyArc-350@users.noreply.github.com>
2023-11-16 22:19:26 +0700

    testing
    not yet

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+          primary: '#525FE1',
+          secondary: '#64748b',
+          content: '#FFF6F4',
+          bgs:'#060047',
+      },
+      grayscale: {
+        on: '#525FE1',
+      }
+    }
+  }
+}
